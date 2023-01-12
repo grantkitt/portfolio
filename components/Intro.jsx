332ff05893be4dbc73a81954150f884386b683ad
grantkitt <38022110+grantkitt@users.noreply.github.com>
2023-01-12 15:39:58 -0600

    all

---
+++ components/Intro.jsx
@@ -0,0 +1,19 @@
+const Intro = () => {
+  return (
+    <div className="py-10 w-2/5 mx-auto">
+        <div className="flex flex-row justify-center font-bold text-3xl mx-auto">
+            <a target="_blank" href='https://drive.google.com/file/d/19OIh2KxMwyJnZRUUn7xFo3mpC4LKjtBc/view?usp=sharing'>
+                <h1>RESUME</h1>
+            </a>
+            <h1 className="font-semibold mx-auto">Grant Kitlowski</h1>
+            
+            <h1 className="cursor-pointer">CONTACT</h1>
+            
+        </div>
+        <p className="text-justify text-xl text-black font-medium mx-auto mt-5">Hey I'm Grant Kitlowski. I'm currently a student at the University of Texas at Austin, seeking a degree in Aerospace Engineering with a certificate in Computer Science. I'm a fully self-taught programmer and love all things full-stack devlopment, machine learning, distributed systems, and problem solving. If you have any questions, please contact me! Currently @ Antler working on my company, Gamma.</p>
+    </div>
+   
+  )
+}
+
+export default Intro
